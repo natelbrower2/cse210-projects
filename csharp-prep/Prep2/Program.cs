698a3689e8d964f24e9550cf48352bf879467dd2
Nathan Brower <bro19053@byui.edu>
2023-04-22 14:01:35 -0600

    Wrote the letter grade program in prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+        string letter;
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Great job! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class.");
+        }
     }
 }

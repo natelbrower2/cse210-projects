ccdee8b247d623610470c03f8930a559a4f5d960
Nathan Brower <bro19053@byui.edu>
2023-04-27 00:25:47 -0600

    Put prep 3 and learning 3 back in place.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        string guess = "0";
+        int guessInt = 0;
+
+        Console.Write("What is the magic number? ");
+        guess = Console.ReadLine();
+        guessInt = int.Parse(guess);
+        while (guessInt != magicNumber)
+        {
+            if (guessInt < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessInt > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            guess = Console.ReadLine();
+            guessInt = int.Parse(guess);
+        }
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine("Thanks for playing");
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,29 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
-        string guess = "0";
-        int guessInt = 0;
-
-        Console.Write("What is the magic number? ");
-        guess = Console.ReadLine();
-        guessInt = int.Parse(guess);
-        while (guessInt != magicNumber)
-        {
-            if (guessInt < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (guessInt > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            Console.Write("What is your guess? ");
-            guess = Console.ReadLine();
-            guessInt = int.Parse(guess);
-        }
-        Console.WriteLine("You guessed it!");
-        Console.WriteLine("Thanks for playing");
+        Console.WriteLine("Hello Learning03 World!");
     }
 }
